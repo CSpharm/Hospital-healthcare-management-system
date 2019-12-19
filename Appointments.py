24af7ed78bbb2ff0e53c6ceb508c3e67799d44a1
Wendy Lin <49999157+CSpharm@users.noreply.github.com>
2019-12-20 00:02:08 +0800

    Add files via upload

--- Appointments.py
+++ Appointments.py
@@ -3,108 +3,94 @@ import sqlite3
 import tkinter.messagebox
 
 # connect to the database for appointments
-conn = sqlite3.connect('Database.db')
+conn1 = sqlite3.connect('Database.db')
 
 # create a cursor
-c = conn.cursor()
+c1 = conn1.cursor()
 
-# empty the list to later append the ids from the database
-ids = []
-
-class Window:
-    def __init__(self,m):
-        self.m = m
+class WindowForAppointments:
+    def __init__(self,a):
+        self.a = a
 
         # create a frame
-        self.left = Frame(m, width=600, height = 720, bg='pink')
-        self.left.pack(side=LEFT)
+        self.left2 = Frame(a, width=600, height = 720, bg='pink')
+        self.left2.pack(side=LEFT)
 
-        self.right = Frame(m, width= 600, height = 720, bg = 'lightgreen')
-        self.right.pack(side=RIGHT)
+        self.right2 = Frame(a, width= 600, height = 720, bg = 'lightgreen')
+        self.right2.pack(side=RIGHT)
 
-        # labels
-        self.heading = Label(self.left, text="UCLH Appointments",font = ('arial 40 bold'),fg='black',bg='pink')
-        self.heading.place(x=0,y=0)
+        # heading
+        self.lh2 = Label(self.left2, text="Appointments",font = ('arial 40 bold'),fg='black',bg='pink')
+        self.lh2.place(x=0,y=0)
+
+        # Fixed Labels
+        self.drnameleft = Label(self.left2, text="Doctors", font=('arial 20 bold'), fg='black', bg='pink')
+        self.drnameleft.place(x=0, y=80)
+
+        self.date1 = Label(self.left2, text="Date1", font=('arial 20 bold'), fg='black', bg='pink')
+        self.date1.place(x=250, y=80)
+        self.date2 = Label(self.left2, text="Date2", font=('arial 20 bold'), fg='black', bg='pink')
+        self.date2.place(x=450, y=80)
+
+
+        dr_namedate = c1.execute("SELECT DrName,Date1,Date2 FROM Drbasic")
+        count = 0
+        rb =IntVar()
+        for row in dr_namedate:
+
+            n = row[0]
+            d1 = row[1]
+            d2 = row[2]
+            count += 1
+            self.drname = Label(self.left2, text=n, font=('arial 20 bold'), fg='black', bg='pink')
+            self.drname.place(x=0, y=50 + 70 * count)
+            self.d1 = Radiobutton(self.left2, text=d1, font=('arial 20 bold'),fg='black',bg='pink', variable = count,value=count,command = lambda:self.booking(count))
+            self.d1.place(x=250, y=50 + 70 * count)
+            self.d2 = Radiobutton(self.left2, text=d2, font=('arial 20 bold'), fg='black',bg='pink',variable = count, value=count,command = lambda:self.booking(count))
+            self.d2.place(x=450, y=50 + 70 * count)
 
         # NHS number
-        self.id = Label(self.left, text="NHS number", font=('arial 20 bold'), fg='black', bg='pink')
-        self.id.place(x=0, y=100)
+        self.id = Label(self.right2, text="NHS number", font=('arial 17 bold'), fg='black', bg='lightgreen')
+        self.id.place(x=0, y=50)
 
         # patients' name
-        self.name = Label(self.left, text="Patient's Name", font=('arial 20 bold'), fg='black', bg='pink')
-        self.name.place(x=0,y=170)
-
-        # age
-        self.age = Label(self.left, text= 'Age', font=('arial 20 bold'), fg='black', bg='pink')
-        self.age.place(x=0,y=240)
-
-        # gender
-        self.gender = Label(self.left, text='Gender', font=('arial 20 bold'), fg='black', bg='pink')
-        self.gender.place(x=0, y=310)
-
-        # phone
-        self.phone = Label(self.left, text='Phone', font=('arial 20 bold'), fg='black', bg='pink')
-        self.phone.place(x=0, y=380)
-
-        # address
-        self.address = Label(self.left, text='Address', font=('arial 20 bold'), fg='black', bg='pink')
-        self.address.place(x=0, y=450)
-
-        # Appointment Time
-        self.time = Label(self.left, text='Appointment Time', font=('arial 20 bold'), fg='black', bg='pink')
-        self.time.place(x=0,y=520)
-
-        # entry for all labels
-        self.id_ent = Entry(self.left, width=30)
-        self.id_ent.place(x=200, y=100)
-
-        self.name_ent = Entry(self.left, width=30)
-        self.name_ent.place(x=200, y=170)
-
-        self.age_ent = Entry(self.left, width=30)
-        self.age_ent.place(x=200, y=240)
-
-        self.gender_ent = Entry(self.left, width=30)
-        self.gender_ent.place(x=200, y=310)
-
-        self.phone_ent = Entry(self.left, width=30)
-        self.phone_ent.place(x=200, y=380)
-
-        self.address_ent = Entry(self.left, width=30)
-        self.address_ent.place(x=200, y=450)
+        self.ptname = Label(self.right2, text="Patient's Name:", font=('arial 17 bold'), fg='black', bg='lightgreen')
+        self.ptname.place(x=0,y=100)
 
         # Button to add an appointment
-        self.submit = Button(self.left, text = 'Add appointment', width=20, height=2,bg='white',command = self.add_appointment)
-        self.submit.place(x=250, y=580)
+        self.submit = Button(self.right2, text = 'Add appointment', width=20, height=2,bg='white',command = self.add_appointment)
+        self.submit.place(x=200, y=300)
+
+    # book the appointment once the patient click one of the button
+    def booking(self,count):
+        li_dr_name_date = []
+        dr_namedate2 = c1.execute("SELECT DrName,Date1,Date2 FROM Drbasic")
+        count2 = 0
+        for row in dr_namedate2:
+            li_dr_name_date.append(row[0])
+            count2 +=1
+
+            if int(count) == int(count2):
+                # exact Dr.name
+                self.drnameright = Label(self.right2, text="Dr's Name:  "+ str(row[0]), font=('arial 17 bold'), fg='black',bg='lightgreen')
+                self.drnameright.place(x=0, y=150)
+                # exact Appointment Date
+                self.time = Label(self.right2, text='Appointment Date: '+ str(row[1]), font=('arial 17 bold'), fg='black',bg='lightgreen')
+                self.time.place(x=0, y=200)
+
 
     # Function to call when the button is clicked
     def add_appointment(self):
-        # getting the user inputs
-        self.val1 = self.id_ent.get()
-        self.val2 = self.name_ent.get()
-        self.val3 = self.age_ent.get()
-        self.val4 = self.gender_ent.get()
-        self.val5 = self.phone_ent.get()
-        self.val6 = self.address_ent.get()
-        self.val7 = 1
 
-        if self.val1 == '' or self.val2 == '' or self.val3 =='' or self.val4 == '' or self.val5 == '' or self.val6 == '':
-            tkinter.messagebox.showinfo('Warning','Please fill up all the boxes')
-        else:
-            sql = "INSERT INTO 'appointments' (Id, Name, Age, Gender, Phone, Address,Condition) VALUES(?,?,?,?,?,?,?)"
-            c.execute(sql,(self.val1, self.val2, self.val3, self.val4, self.val5, self.val6, self.val7))
-            conn.commit()
-            tkinter.messagebox.showinfo('Confirmation', 'Appointment for'+ str(self.val2)+ 'has been submitted.')
+        #self.val1 = self.id_ent.get()
+        #self.val2 = self.name_ent.get()
+        self.val3 = 1
 
-# create the object
-root = Tk()
-r = Window(root)
+        #if self.val1 == '' or self.val2 == '' or self.val3 =='' or self.val4 == '' or self.val5 == '' or self.val6 == '':
+            #tkinter.messagebox.showinfo('Warning','Please fill up all the boxes')
+        #else:
+            #sql = "INSERT INTO 'appointments' (Id, Name, Age, Gender, Phone, Address,Condition) VALUES(?,?,?,?,?,?,?)"
+            #c.execute(sql,(self.val1, self.val2, self.val3, self.val4, self.val5, self.val6, self.val7))
+            #conn.commit()
+            #tkinter.messagebox.showinfo('Confirmation', 'Appointment for'+ str(self.val2)+ 'has been submitted.')
 
-# resolution of the window
-root.geometry('1200x720+0+0')
-
-# preventing the resize feature
-root.resizable(False,False)
-
-# end the loop
-root.mainloop()
